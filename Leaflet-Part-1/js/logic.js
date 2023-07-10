e36f901bca6e99258210a10380b61f473a948cb5
DianaGRM <diana.gramirezm@gmail.com>
2023-07-09 21:22:24 -0600

    update code

--- Leaflet-Part-1/js/logic.js
+++ Leaflet-Part-1/js/logic.js
@@ -17,6 +17,20 @@ function createMap(earthquakes) {
   }
 
   //create markers
+  function markerSize(population) {
+    return (population) * 10000;
+  }
+  function markerColor(color) {
+    let num = Math.ceil(color)
+    
+    var b = num & 0xFF,
+        g = (num & 0xFF00) >>> 8,
+        r = (num & 0xFF0000) >>> 16,
+        a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
+    return "rgb(" + [r, g, b].join(",") + ")";
+    
+    //return (`#${num.toString(16).padStart(6,"0")}`);
+  }
 
 d3.json(url).then(createMarkers);
 
@@ -26,11 +40,14 @@ function createMarkers(response){
     for (let i = 0; i < response.features.length; i++) {
         let earthquake = response.features[i];
 
-        coord = [earthquake.geometry.coordinates[0],earthquake.geometry.coordinates[1]]
+        coord = [earthquake.geometry.coordinates[1],earthquake.geometry.coordinates[0]]
 
         let marker = L.circle(coord,{
-            color: earthquake.geometry[2],
-            radius: earthquake.properties.mag
+            color: "#000000",
+            fillcolor: markerColor(earthquake.geometry.coordinates[2]),
+            opacity: 1,
+            fillOpacity: 1,
+            radius: markerSize(earthquake.properties.mag)
              });
 
         earthquakeMarkers.push(marker);
@@ -40,6 +57,6 @@ function createMarkers(response){
     let earthquakes = L.layerGroup(earthquakeMarkers);
     createMap(earthquakes);
 
-    console.log(coord)
+    console.log(markerColor(response.features[0].geometry.coordinates[2]))
 }
 
